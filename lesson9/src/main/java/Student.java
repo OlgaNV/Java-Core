c3a3c48e781eeba378eb484909664087ccc65233
OlgaNV <93245074+OlgaNV@users.noreply.github.com>
2022-01-03 16:58:44 +0500

    Add files via upload

---
+++ lesson9/src/main/java/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private int course;
+    private String name;
+
+    public Student(int course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
